26bed1d6a2bc96ab30ecdd50393501be391f5166
huy thai <huythaia123@gmail.com>
2023-03-30 13:50:52 +0700

    fig get started page

---
+++ app/src/main/java/com/example/btl_app_android/GetStarted.kt
@@ -0,0 +1,26 @@
+package com.example.btl_app_android
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_get_started.*
+
+class GetStarted : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_get_started)
+
+        addClickEvents()
+    }
+
+    private fun addClickEvents() {
+        handleBtnGetStarted()
+    }
+
+    private fun handleBtnGetStarted() {
+        btnGetStarted.setOnClickListener {
+            val gotoMainActivity = Intent(this, MainActivity::class.java)
+            startActivity(gotoMainActivity)
+        }
+    }
+}
