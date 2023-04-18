05c4f4fbd2634f18c0ecfedef6a8d0d5a6b9ec17
huy thai <huythaia123@gmail.com>
2023-04-18 14:43:56 +0700

    upload value string

--- app/src/main/java/com/example/btl_app_android/MainActivity.kt
+++ app/src/main/java/com/example/btl_app_android/MainActivity.kt
@@ -28,11 +28,15 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        navBar()
 
+        navBar()
+        renderListRecipe()
+
+    }
+
+    private fun renderListRecipe() {
         builderDialog = ProgressDialog(this, "Loading ...")
         builderDialog.show()
-
         manager = RequestManager(this)
         manager.getRandomRecipe(randomRecipeResponseListener)
     }
