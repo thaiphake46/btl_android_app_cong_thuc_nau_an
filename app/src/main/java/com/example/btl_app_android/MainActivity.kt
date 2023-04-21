5a0ec590c93c52b8248e12f10b8638893aa6b22e
huy thai <huythaia123@gmail.com>
2023-04-21 11:55:55 +0700

    use tagadapter in home fragment

--- app/src/main/java/com/example/btl_app_android/MainActivity.kt
+++ app/src/main/java/com/example/btl_app_android/MainActivity.kt
@@ -1,25 +1,25 @@
 package com.example.btl_app_android
 
-import android.app.AlertDialog
+import android.graphics.PorterDuff
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ProgressBar
 import android.widget.Toast
 import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.btl_app_android.adapters.RandomRecipeAdapter
+import com.example.btl_app_android.adapters.RvTagsInterface
+import com.example.btl_app_android.adapters.TagsAdapter
 import com.example.btl_app_android.dialogs.ProgressDialog
 import com.example.btl_app_android.mainFragment.NavBarMainAdapter
 import com.google.android.material.tabs.TabLayoutMediator
 import com.example.btl_app_android.listeners.RandomRecipeResponseListener
 import com.example.btl_app_android.models.RandomRecipeApiResponse
+import com.google.android.material.tabs.TabLayout
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var dialog: AlertDialog
-    private lateinit var progressBar: ProgressBar
-
     private lateinit var manager: RequestManager
     lateinit var randomRecipeAdapter: RandomRecipeAdapter
     lateinit var recyclerView: RecyclerView
@@ -31,7 +31,23 @@ class MainActivity : AppCompatActivity() {
 
         navBar()
         renderListRecipe()
+    }
 
+    private fun tagsRecipe() {
+        val tagsType = resources.getStringArray(R.array.tags).toList()
+        val recycler_tags: RecyclerView = findViewById(R.id.recycler_tags)
+        recycler_tags.setHasFixedSize(true)
+        recycler_tags.layoutManager = LinearLayoutManager(
+            this@MainActivity,
+            LinearLayoutManager.HORIZONTAL,
+            false
+        )
+        val adapter = TagsAdapter(tagsType, object : RvTagsInterface {
+            override fun onCLickTag(pos: Int) {
+                Toast.makeText(this@MainActivity, tagsType[pos], Toast.LENGTH_SHORT).show()
+            }
+        })
+        recycler_tags.adapter = adapter
     }
 
     private fun renderListRecipe() {
@@ -48,22 +64,125 @@ class MainActivity : AppCompatActivity() {
             when (pos) {
                 0 -> {
                     tab.icon = ContextCompat.getDrawable(this, R.drawable.home_24px)
+                    tab.icon?.setColorFilter(
+                        ContextCompat.getColor(
+                            this@MainActivity,
+                            R.color.black
+                        ), PorterDuff.Mode.SRC_IN
+                    )
                 }
                 1 -> {
                     tab.icon = ContextCompat.getDrawable(this, R.drawable.bookmark_24px)
+                    tab.icon?.setColorFilter(
+                        ContextCompat.getColor(
+                            this@MainActivity,
+                            R.color.textcl
+                        ), PorterDuff.Mode.SRC_IN
+                    )
                 }
                 2 -> {
                     tab.icon = ContextCompat.getDrawable(this, R.drawable.shopping_bag_24px)
+                    tab.icon?.setColorFilter(
+                        ContextCompat.getColor(
+                            this@MainActivity,
+                            R.color.textcl
+                        ), PorterDuff.Mode.SRC_IN
+                    )
                 }
                 3 -> {
                     tab.icon = ContextCompat.getDrawable(this, R.drawable.person_24px)
+                    tab.icon?.setColorFilter(
+                        ContextCompat.getColor(
+                            this@MainActivity,
+                            R.color.textcl
+                        ), PorterDuff.Mode.SRC_IN
+                    )
                 }
             }
         }.attach()
+
+        navbarMain.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab) {
+                when (tab.position) {
+                    0 -> {
+                        tab.icon?.setColorFilter(
+                            ContextCompat.getColor(
+                                this@MainActivity,
+                                R.color.black
+                            ), PorterDuff.Mode.SRC_IN
+                        )
+                    }
+                    1 -> {
+                        tab.icon?.setColorFilter(
+                            ContextCompat.getColor(
+                                this@MainActivity,
+                                R.color.black
+                            ), PorterDuff.Mode.SRC_IN
+                        )
+                    }
+                    2 -> {
+                        tab.icon?.setColorFilter(
+                            ContextCompat.getColor(
+                                this@MainActivity,
+                                R.color.black
+                            ), PorterDuff.Mode.SRC_IN
+                        )
+                    }
+                    3 -> {
+                        tab.icon?.setColorFilter(
+                            ContextCompat.getColor(
+                                this@MainActivity,
+                                R.color.black
+                            ), PorterDuff.Mode.SRC_IN
+                        )
+                    }
+                }
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab) {
+                when (tab.position) {
+                    0 -> {
+                        tab.icon?.setColorFilter(
+                            ContextCompat.getColor(
+                                this@MainActivity,
+                                R.color.textcl
+                            ), PorterDuff.Mode.SRC_IN
+                        )
+                    }
+                    1 -> {
+                        tab.icon?.setColorFilter(
+                            ContextCompat.getColor(
+                                this@MainActivity,
+                                R.color.textcl
+                            ), PorterDuff.Mode.SRC_IN
+                        )
+                    }
+                    2 -> {
+                        tab.icon?.setColorFilter(
+                            ContextCompat.getColor(
+                                this@MainActivity,
+                                R.color.textcl
+                            ), PorterDuff.Mode.SRC_IN
+                        )
+                    }
+                    3 -> {
+                        tab.icon?.setColorFilter(
+                            ContextCompat.getColor(
+                                this@MainActivity,
+                                R.color.textcl
+                            ), PorterDuff.Mode.SRC_IN
+                        )
+                    }
+                }
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab) {}
+        })
     }
 
     private val randomRecipeResponseListener = object : RandomRecipeResponseListener {
         override fun didFetch(response: RandomRecipeApiResponse?, message: String?) {
+            tagsRecipe()
             recyclerView = findViewById(R.id.recycler_random)
             recyclerView.setHasFixedSize(true)
             recyclerView.layoutManager = GridLayoutManager(this@MainActivity, 1)
--- app/src/main/java/com/example/btl_app_android/adapters/TagsAdapter.kt
+++ app/src/main/java/com/example/btl_app_android/adapters/TagsAdapter.kt
@@ -1,29 +1,30 @@
 package com.example.btl_app_android.adapters
 
-import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.btl_app_android.R
 import kotlinx.android.synthetic.main.tag_item.view.*
 
-class TagsAdapter(var context: Context,private val tags: List<String>) : RecyclerView.Adapter<TagsAdapter.ItemViewHolder>() {
-    inner class ItemViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        var txt_tag = itemView.findViewById<TextView>(R.id.txt_tag)
-        init {
-            txt_tag = txt_tag
-        }
-    }
+class TagsAdapter(private val tags: List<String>, val onCLickTags: RvTagsInterface) :
+    RecyclerView.Adapter<TagsAdapter.ItemViewHolder>() {
+    inner class ItemViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
-        val view = LayoutInflater.from(context).inflate(R.layout.tag_item, parent, false)
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.tag_item, parent, false)
         return ItemViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
-        holder.txt_tag.text = tags[position]
+        holder.itemView.apply {
+            txt_tag.text = tags[position]
+
+            // click
+            holder.itemView.setOnClickListener{
+                onCLickTags.onCLickTag(position)
+            }
+        }
     }
 
     override fun getItemCount(): Int {
